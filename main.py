04891d90368e8625de3f30eeda6db033ca09c149
Joseph Bosio <bosiojos001@warren.k12.in.us>
2024-11-22 09:19:09 -0500

    initial commmit

---
+++ main.py
@@ -0,0 +1,12 @@
+#ToDO : Ask the user for how many rows.
+rows = int(input("Enter the number of rows: "))
+# And store it in a variable call rows.
+
+for row in range(1, rows + 1):
+    for star in range(1, row + 1):
+        #TODO: print a * and a space.
+        print("*", end="")
+        #add end=""
+        # example print("stuff", end=""
+
+    print()
